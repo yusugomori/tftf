5bc8654b112f374474afb1062d637a136136e2dd
Yusuke Sugomori <me@yusugomori.com>
2018-08-12 12:14:09 +0900

    update Conv2D

--- tftf/layers/Conv2D.py
+++ tftf/layers/Conv2D.py
@@ -19,10 +19,19 @@ class Conv2D(Layer):
         if len(kernel_size) != 3:
             raise ValueError('Dimension of kernel_size must be 3.')
 
-        padding = padding.upper()
+        if len(strides) != 2:
+            raise ValueError('Dimension of strides must be 2.')
 
-        self.input_dim = None
-        self.output_dim = None
+        padding = padding.upper()
+        if padding not in ('VALID', 'SAME'):
+            raise ValueError('padding must be one of \'valid\' or \'same\'.')
+
+        self.input_dim = input_shape
+        self.output_dim = self._get_output_shape()
+        self.kernel_size = kernel_size
+        self.strides = strides
+        self.padding = padding
+
         kernel_shape = kernel_size[:2] + (input_shape[2], kernel_size[2])
 
         self.W = self.kernel_initializer(initializer,
@@ -30,15 +39,36 @@ class Conv2D(Layer):
                                          name='W')
         self.b = zeros((output_dim), name='b')
 
-        self.strides = strides
-        self.padding = padding
-
     def __repr__(self):
         return '<{}: shape({}, {})>'.format('Dense',
                                             self.input_dim,
                                             self.output_dim)
 
+    @property
+    def input_shape(self):
+        return self.input_dim
+
+    @property
+    def output_shape(self):
+        return self.output_dim
+
     def forward(self, x):
         return tf.nn.conv2d(x, self.W,
                             strides=self.strides,
                             padding=self.padding)
+
+    def _get_output_shape(self):
+        input_shape = self.input_shape
+        kernel_size = self.kernel_size
+        strides = self.strides
+        padding = self.padding
+
+        image_size = input_shape[:2]
+        channel = kernel_size[2]
+
+        if padding is 'SAME':
+            return tuple(list(image_size) + [channel])
+        else:
+            return tuple(list(int((np.array(image_size)
+                                  - np.array(kernel_size))
+                                  / np.array(strides)) + 1) + [channel])
