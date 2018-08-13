b06742fa5782cb84cb109f6a70910b61afd1523b
Yusuke Sugomori <me@yusugomori.com>
2018-08-13 22:30:15 +0900

    release v0.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='tftf',
-    version='0.0.4',
+    version='0.0.5',
     description='TensorFlow TransFormer',
     author='Yusuke Sugomori',
     author_email='me@yusugomori.com',
