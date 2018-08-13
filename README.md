390d6ff35b72f39616c6431818321d4bd2fe1894
Yusuke Sugomori <yusuke_sugomori@micin.jp>
2018-08-13 16:28:38 +0900

    add PyPI

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# tftf
-TensorFlow TransFormer🍔
+# TFTF: TensorFlow TransFormer🍔
+
+TensorFlow for everybody.
+
+## Quick glance
+
+```python
+from tftf.layers import Layer, Dense, Activation
+from tftf.models import Model
+
+'''
+Build model
+'''
+model = Model()
+model.add(Dense(784, 500))
+model.add(Activation('sigmoid'))
+model.add(Dense(500, 10))
+model.add(Activation('softmax'))
+model.compile()
+
+model.describe()
+
+'''
+Train model
+'''
+model.fit(train_X, train_y)
+
+'''
+Test model
+'''
+print(model.accuracy(test_X, test_y))
+```
+
+See [examples](https://github.com/yusugomori/tftf/tree/master/examples) for other implementations.
+
+## Installation
+
+- **Install TFTF from PyPI (recommended):**
+
+```sh
+pip install tensorflow
+pip install tftf
+```
+
+- **Alternatively: install TFTF from the GitHub source:**
+
+First, clone TFTF using `git`:
+
+```sh
+git clone https://github.com/yusugomori/tftf.git
+```
+
+ Then, `cd` to the TFTF folder and run the install command:
+```sh
+cd tftf
+sudo python setup.py install
+```
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='tftf',
+    version='0.0.2',
+    description='TensorFlow TransFormer',
+    author='Yusuke Sugomori',
+    author_email='me@yusugomori.com',
+    url='https://github.com/yusugomori/tftf',
+    download_url='',
+    license='Apache 2.0',
+    install_requires=['numpy>=1.13.3'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='tensorflow keras machine deep learning',
+    packages=find_packages()
+)
